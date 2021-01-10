2f3e83328cf354ff6b244514e73c2cf574f8a789
Dzianis Zavoiskikh <6557587@gmail.com>
2021-01-10 16:40:26 +0300

    Next Hello

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Hello Dzianis!!)) It works on AWS!'));
+app.get('/', (req, res) => res.send('Hello World! Hello Dzianis!!)) It works on AWS! Greate!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
